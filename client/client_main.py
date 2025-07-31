2360e89fc3675528f05c7bc207fb5af7099b8585
berkaytor <berkaytor19901003@gmail.com>
2025-07-31 12:44:29 -0400

    update version 2

--- client/client_main.py
+++ client/client_main.py
@@ -1,19 +1,17 @@
-# client_main.py
-
 import time
 from client_screenshot import capture_screenshots
 from client_network import send_screenshot
 
 
 def main():
-    host = "127.0.0.1:8081"
-    interval = 4
+    host = "localhost:8081"
+    # host = "192.168.1.118:8081"
+    interval = 5
 
     while True:
         shots = capture_screenshots(max_monitors=2)
         for mon, img_bytes in shots:
             send_screenshot(host, mon, img_bytes)
-            # cache_screenshot(mon, img_bytes)
         time.sleep(interval)
 
 
--- client/client_network.py
+++ client/client_network.py
@@ -1,18 +1,20 @@
-# client_network.py
-
 import requests
 import getpass
+import socket
 
 username = getpass.getuser()
+hostname = socket.gethostname()
 
 
 def send_screenshot(host_url, monitor_num, image_bytes):
-    url = f"http://{host_url}/{username}/upload"
+    url = f"http://{host_url}/{username}_{hostname}/{monitor_num}/upload"
     try:
         files = {
-            "screenshot": (f"monitor{monitor_num}.webp", image_bytes, "image/webp"),
+            "screenshot": (f"screen.webp", image_bytes, "image/webp"),
         }
         resp = requests.post(url, files=files, timeout=10)
-        return resp.status_code == 200
+        status = resp.status_code
+        print(f"Status code {status}")
+        return status == 200
     except requests.RequestException:
         return False
